ddd09545097f1b56251519d7959474a525bc0a62
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-05-30 15:08:48 -0700

    Day 1

---
+++ Arrays_and_Hashing/242_Valid_Anagram.java
@@ -0,0 +1,33 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ */
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // Returning false if lenths are equal
+        if (s.length() != t.length()) return false;
+        // Intialize a array to store values of alphabets
+        int[] charStore = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            charStore[s.charAt(i) - 'a']++;
+            charStore[t.charAt(i) - 'a']--;
+        }
+
+        for (int n: charStore) if (n != 0) return false;
+
+        return true;
+    }
+}
