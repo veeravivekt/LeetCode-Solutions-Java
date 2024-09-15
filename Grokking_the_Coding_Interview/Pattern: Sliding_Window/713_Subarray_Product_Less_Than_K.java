bb48d4e2f5c9eb82a1e935b13a6e35f7073cd104
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-09-15 13:15:11 -0700

    sub array product less than k

---
+++ Grokking_the_Coding_Interview/Pattern: Sliding_Window/713_Subarray_Product_Less_Than_K.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k == 0) return 0;
+        int count = 0;
+        int left = 0;
+        int right = 0;
+        int prod = 1;
+        while (right < nums.length) {
+            prod *= nums[right];
+            while (prod >= k) prod /= nums[left++];
+            count += (right - left + 1);
+            right++;
+        }
+        return count;
+    }
+}
