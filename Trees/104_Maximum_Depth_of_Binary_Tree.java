8870e58136ac69ea7365806000f0c07366ee54a1
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-09-20 18:02:24 -0700

    Depth of Binary Tree - Iteration BFS

--- Trees/104_Maximum_Depth_of_Binary_Tree.java
+++ Trees/104_Maximum_Depth_of_Binary_Tree.java
@@ -5,3 +5,24 @@ class Solution {
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 }
+
+
+// Iteration - BFS
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int count = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int len = q.size();
+            for (int i = 0; i < len; i++) {
+                if (q.peek().left != null) q.offer(q.peek().left);
+                if (q.peek().right != null) q.offer(q.peek().right);
+                q.poll();
+            }
+            count++;
+        }
+        return count;
+    }
+}
