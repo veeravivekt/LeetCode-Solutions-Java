99f4c5aa3eba5067c934314687a68c2333275b24
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-09-25 12:33:55 -0700

    invert binarytree

---
+++ Trees/226_Invert_Binary_Tree.java
@@ -0,0 +1,9 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode node = new TreeNode(root.val);
+        node.left = invertTree(root.right);
+        node.right = invertTree(root.left);
+        return node;
+    }
+}
