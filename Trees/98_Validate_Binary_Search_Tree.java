c4ca22955f143efdee9c36939a3bab303b4ec7e5
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-09-28 16:31:27 -0700

    one line removed from validate BST

--- Trees/98_Validate_Binary_Search_Tree.java
+++ Trees/98_Validate_Binary_Search_Tree.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        if (root == null) return true;
         return dfs(root, null, null);
     }
     public boolean dfs(TreeNode root, Integer min, Integer max) {
         if (root == null) return true;
-        if (min != null && root.val <= min || max != null && root.val >= max) return false;
+        if ((min != null && root.val <= min) || (max != null && root.val >= max)) return false;
         return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
     }
 }
