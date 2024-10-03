8e5a175643dcbd586ec9ec9f57bf2fa50ee60ec7
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-10-03 12:47:31 -0700

    udpate same tree

--- Trees/100_Same_Tree.java
+++ Trees/100_Same_Tree.java
@@ -1,14 +1,7 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return dfs(p, q);
-    }
-    public boolean dfs(TreeNode p, TreeNode q) {
         if (p == null && q == null) return true;
-        if (p == null || q == null) return false;
-        if (p.val != q.val) return false;
-
-        boolean left = dfs(p.left, q.left);
-        boolean right = dfs(p.right, q.right);
-        return left && right;
+        if (p == null || q == null || p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
